55e3b6153f29631e762d2179f2e4199243d13b81
Elm <edillorluke@gmail.com>
2024-05-18 20:46:33 +0800

    updated Stats

--- Game/Scenes/Entity/Script/Stats.cs
+++ Game/Scenes/Entity/Script/Stats.cs
@@ -5,13 +5,11 @@ public partial class Stats : Node2D
 	[Export]
 	public int Health = 100;
 	[Export]
-	public float Speed = 100f;
+	public float Speed = 250f;
 	[Export]
 	public float SprintFactor = 1.5f;
 	[Export]
 	public float Accel = 0.35f;
-	[Export]
-	public float Decel = 0.5f;
 	[Export]
 	public int Damage = 1;
 }
